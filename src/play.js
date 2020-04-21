9f9e54ab7b17a6e1008e2bb289a76fe0f13d3920
Krzysztof Kozak <hithard5@gmail.com>
2020-04-21 15:29:44 +0200

    checkpoint

--- src/play.js
+++ src/play.js
@@ -26,103 +26,12 @@ export default function Play() {
       </section>
 
       <section className="playerHand">
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[0].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[0].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
 
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[1].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[1].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
+      <div className="cardContainer">
+      
+      </div>
 
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[2].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[2].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
 
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[3].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[3].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
-
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[4].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[4].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
-
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[5].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[5].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
-
-        <div className="card-container">
-          <button>Discard</button>
-          <p>Total Value: {cards[6].basePoints}</p>
-          <div
-            style={{
-              background: `url(./deck/${cards[6].image})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="card"
-          ></div>
-        </div>
       </section>
     </div>
   );
