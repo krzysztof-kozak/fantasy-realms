9357f75ae4daea8493ce28fa0da717b7b5c3eb51
Krzysztof Kozak <hithard5@gmail.com>
2020-04-24 11:26:37 +0200

    Updates

---
+++ precache-manifest.6f8e6a0ed77503a5a7b02766d3b78ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0724feb4ec2d20e5d65ff3c7f22a290",
+    "url": "/Fantasy-Realms/index.html"
+  },
+  {
+    "revision": "cd02e34f0a17d0ea2862",
+    "url": "/Fantasy-Realms/static/css/main.9ccfa86c.chunk.css"
+  },
+  {
+    "revision": "800b46740c6410d6c40f",
+    "url": "/Fantasy-Realms/static/js/2.e5739a84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Fantasy-Realms/static/js/2.e5739a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd02e34f0a17d0ea2862",
+    "url": "/Fantasy-Realms/static/js/main.17f28b60.chunk.js"
+  },
+  {
+    "revision": "e6e24371d142a8033939",
+    "url": "/Fantasy-Realms/static/js/runtime-main.f2371ac7.js"
+  }
+]);
