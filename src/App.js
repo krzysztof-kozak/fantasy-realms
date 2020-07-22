3a71a447989ad0d78804f363456ab24823f162fb
Krzysztof Kozak <hithard5@gmail.com>
2020-07-22 19:39:56 +0200

    fixing display issues wip

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
-import GoogleFontLoader from "react-google-font-loader";
-import LandingPage from "./landingPage";
-import Play from "./play";
-import Rules from "./rules";
-import About from "./about";
-import NotFound from "./notFound";
+import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import GoogleFontLoader from 'react-google-font-loader';
+import LandingPage from './landingPage';
+import Play from './play';
+import Rules from './rules';
+import About from './about';
+import NotFound from './notFound';
 
 const App = () => (
   <HashRouter>
     <GoogleFontLoader
       fonts={[
         {
-          font: "Pacifico",
+          font: 'Pacifico',
           weights: [400],
         },
       ]}
     />
     <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/play" component={Play} />
-      <Route path="/rules" component={Rules} />
-      <Route path="/about" component={About} />
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/play' component={Play} />
+      <Route path='/rules' component={Rules} />
+      <Route path='/about' component={About} />
       <Route component={NotFound} />
     </Switch>
   </HashRouter>
--- src/landingPage.js
+++ src/landingPage.js
@@ -1,50 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../src/css/landingPage.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../src/css/landingPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function landingPage() {
   return (
-    <div className="container">
-      <header className="box">
+    <div className='container'>
+      <header className='box'>
         <h1>Fantasy Realms</h1>
       </header>
+      <div className='section-wrapper'>
+        <section className='play box'>
+          <Link className='link' to='/play'>
+            Play
+          </Link>
+        </section>
 
-      <section className="play box">
-        <Link className="link" to="/play">
-          Play
-        </Link>
-      </section>
+        <section className='rules box'>
+          <Link className='link' to='/rules'>
+            Rules
+          </Link>
+        </section>
 
-      <section className="rules box">
-        <Link className="link" to="/rules">
-          Rules
-        </Link>
-      </section>
+        <section className='about box'>
+          <Link className='link' to='/about'>
+            About
+          </Link>
+        </section>
+      </div>
 
-      <section className="about box">
-        <Link className="link" to="/about">
-          About
-        </Link>
-      </section>
-
-      <footer className="box">
+      <footer className='box'>
         <p> Made by Krzysztof Kozak</p>
-        <a
-          href="https://www.linkedin.com/in/kriskozak/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon className="linkedIn" icon={faLinkedin} />
+        <a href='https://www.linkedin.com/in/kriskozak/' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon className='linkedIn' icon={faLinkedin} />
         </a>
-        <a
-          href="https://github.com/Kris-c0der/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon className="linkedIn" icon={faGithub} />
+        <a href='https://github.com/Kris-c0der/' target='_blank' rel='noopener noreferrer'>
+          <FontAwesomeIcon className='linkedIn' icon={faGithub} />
         </a>
       </footer>
     </div>
